9bd2d620cfd4df5d9fa9bf3fac1f77992a4a6e87
Krystian Czaplicki <krystianczaplicki@outlook.com>
2024-02-24 15:13:25 +0100

    Added RabbitMQ basic queue sample project

---
+++ HandsOn.Console.RabbitMQ/HandsOn.Console.RabbitMQ.QueueConsumer/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Text;
+
+Console.WriteLine("HandsOn.Console.RabbitMQ.Queue - Consumer");
+
+const string queueName = "basic_queue";
+
+var factory = new ConnectionFactory { HostName = "localhost" };
+
+using var connection = factory.CreateConnection();
+using var channel = connection.CreateModel();
+
+channel.QueueDeclare(queueName, false, false, false, null);
+
+var consumer = new EventingBasicConsumer(channel);
+
+consumer.Received += (_, eventArgs) =>
+{
+    var message = Encoding.UTF8.GetString(eventArgs.Body.ToArray());
+    Console.WriteLine($"[{DateTime.Now.ToLocalTime()}] Received '{message}'");
+};
+
+channel.BasicConsume(queueName, true, consumer);
+
+Console.WriteLine("\nPress any key to exit.\n");
+Console.ReadKey();
---
+++ HandsOn.Console.RabbitMQ/HandsOn.Console.RabbitMQ.QueueProducer/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Text;
+
+Console.WriteLine("HandsOn.Console.RabbitMQ.Queue - Producer");
+
+if (args.Length != 2)
+{
+    Console.WriteLine($"\nInvalid parameters count.\n\n  Usage: ./{args[0]} <message>\n");
+    return;
+}
+
+var message = args[1];
+
+const string queueName = "basic_queue";
+
+var factory = new ConnectionFactory { HostName = "localhost" };
+
+using var connection = factory.CreateConnection();
+using var channel = connection.CreateModel();
+
+channel.QueueDeclare(queueName, false, false, false, null);
+
+var messageBytes = Encoding.UTF8.GetBytes(message);
+channel.BasicPublish(string.Empty, queueName, null, messageBytes);
+
+Console.WriteLine($"[{DateTime.Now.ToLocalTime()}] Sent '{message}'\n");
